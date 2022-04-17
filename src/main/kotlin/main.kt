66a79f083175dd3da5a80c9bb7e02606e55cdf98
jud-ie19 <owuorakinyijudith@gmail.com>
2022-04-17 19:17:31 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,32 @@
+fun main(){
+  val language= arrayOf("ruby","kotlin","python","java")
+   for (item in language)
+        println(item)
+    for (item in language.indices){
+        //printing array elements having index only
+        if (item% 2==0){
+            println(language[item])
+     val name="judith"
+     for (letter in name) {
+         println(name)
+         for (letter in name.indices){
+             println(name[letter])
+         for (i in 1..4)
+             println(i)
+             println("for(i in 4 downTo 1) println i= ")
+             for (i in 4 downTo 1) print(i)
+             println()
+
+             }
+
+             }
+
+
+
+     }
+        }
+    }
+
+
+
+
